b4ee244bc995949ded85cbff9c0369121a4531f3
bulagorns <5610520172@student.tu.ac.th>
2017-03-29 15:49:09 +0700

    setting url

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
 
     # TEST
     #account
-    url(r'^', login_view, name='login'),
+    url(r'^login/', login_view, name='login'),
     url(r'^register/', register_view, name='register'),
     url(r'^logout/', logout_view, name='logout'),
 
